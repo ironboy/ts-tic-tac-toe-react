931aa39cb7fe7e8381d8aee8873f15bbab9e1175
Ironboy <ironboy@users.noreply.github.com>
2024-08-29 10:32:45 +0200

    basic gameplay working

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,24 @@
+import { useState } from 'react';
+import BoardClass from './classes/Board';
+
 export default function App() {
-  return <>I am a React App.</>;
+
+  // create a state - an object
+  // in which can update each property as we want
+  // by calling setState
+  const [state, _setState] = useState({
+    board: new BoardClass(() => setState())
+  });
+
+  const setState = (prop: string = '', value: any = '') => {
+    _setState({ ...state, [prop]: value });
+  }
+
+  if (state.board.winner) {
+    alert(state.board.winner + ' has won');
+  }
+
+  return <>
+    {state.board.render()}
+  </>;
 }
